c35f8f70c923fca0feb965cde59ebca6eee5aaa7
Eric Matthys <ericmatthys@gmail.com>
2022-07-14 13:00:43 -0600

    timer test

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,28 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export const config = {
   matcher: ['/'],
 };
 
-export async function middleware(request: NextRequest): Promise<NextResponse> {
-  const timeoutID = globalThis.setTimeout(() => {
-    globalThis.console.log('timeout');
-  }, 1000);
+function timerTest() {
+  return new Promise<void>((resolve) => {
+    console.log('setting timers');
 
-  globalThis.clearTimeout(timeoutID);
+    const timeout1 = globalThis.setTimeout(() => {
+      // This callback won't be called since timeout2 cancels it.
+      console.log('timeout 1: should be unreachable!');
+    }, 8000);
+  
+    globalThis.setTimeout(() => {
+      console.log('timeout 2');
+      globalThis.clearTimeout(timeout1);
+      resolve();
+    }, 3000);  
+  })  
+}
 
-  return NextResponse.next();
+export async function middleware(request: NextRequest, event: NextFetchEvent): Promise<NextResponse> {
+  event.waitUntil(timerTest());
+
+  return NextResponse.next()
 }
